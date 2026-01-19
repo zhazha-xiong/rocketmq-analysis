876b495913a3dddd9ab38aa7737572e7b17d65bb
zhazha-xiong <abc2289328233@163.com>
2026-01-19 23:41:21 +0800

    refactor: 扩大了日期的范围，修改了相应的代码

--- scripts/module_b/analyze_overtime.py
+++ scripts/module_b/analyze_overtime.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from chinese_calendar import is_holiday
+from matplotlib.patches import Patch
 
 def main() -> None:
     repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
@@ -87,14 +88,15 @@ def main() -> None:
     plt.figure(figsize=(10, 8))
     labels = [f'加班节假日 ({holidays_worked}天)', f'休息节假日 ({holidays_rested}天)']
     sizes = [holidays_worked, holidays_rested]
-
     colors = ['#ff9999', '#99ff99'] 
-    explode = (0.1, 0)  # 突出显示加班部分
-    
-    wedges = plt.pie(sizes, explode=explode, labels=None, colors=colors, autopct='%1.1f%%',
+    explode = (0.1, 0)
+    wedges, texts, autotexts = plt.pie(sizes, explode=explode, labels=None, colors=colors, autopct='%1.1f%%',
             shadow=True, startangle=140)
+    
+    legend_elements = [Patch(facecolor=colors[0], label=labels[0]),
+                       Patch(facecolor=colors[1], label=labels[1])]
             
-    plt.legend(wedges, labels, title="统计详情", loc="center left", bbox_to_anchor=(0.85, 0.5))
+    plt.legend(handles=legend_elements, title="统计详情", loc="center left", bbox_to_anchor=(0.85, 0.5))
 
             
     plt.title(f'节假日加班比例分析\n({min_date} ~ {max_date})')
